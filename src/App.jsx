14d8c722b99619ce3681de2f47eb47d8b3ff3196
holatodos14 <131733759+holatodos14@users.noreply.github.com>
2024-07-26 00:54:29 -0500

    problems with footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products.jsx'
 import { Header } from './components/Header.jsx'
-import { Footer } from './components/Footer.jsx'
-import { IS_DEVELOPMENT } from './config.js'
 import { useFilters } from './hooks/useFilters.js'
 import { Cart } from './components/Cart.jsx'
 import { CartProvider } from './context/cart.jsx'
@@ -17,7 +15,6 @@ function App () {
       <Header />
       <Cart />
       <Products products={filteredProducts} />
-      {IS_DEVELOPMENT && <Footer />}
     </CartProvider>
   )
 }
